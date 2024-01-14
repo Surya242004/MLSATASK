d524ee5eadd7e7111d174bd76dece9305a7aa091
SuryaPrakash <131835756+Surya242004@users.noreply.github.com>
2024-01-14 16:55:04 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/nature-3082832_1280.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "natural background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a AI student studying at Sri Sairam Engineering College. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
